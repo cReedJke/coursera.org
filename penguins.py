fb8770dfd6dade444fbfddacc6aec4f5005f3119
cReedJke <71238088+cReedJke@users.noreply.github.com>
2020-09-14 03:29:57 +0300

    Add files via upload

---
+++ penguins.py
@@ -0,0 +1,11 @@
+a = '   _~_     '
+b = '  (o o)    '
+c = ' /  V  \\   '
+d = '/(  _  )\\  '
+e = '  ^^ ^^    '
+count = int(input())
+print(a * count)
+print(b * count)
+print(c * count)
+print(d * count)
+print(e * count)
